4a2c270f5d66a808c19be5cc91dbc92d3e7f0ca3
DenDen047 <sh.mn.nat@gmail.com>
2016-04-02 23:18:35 +0900

    first commit

---
+++ scripts/dropbooks.js
@@ -0,0 +1,30 @@
+var MarkTitle = class {
+    getInfo () {
+        this.books =  document.getElementsByClassName("list");
+    }
+
+    mark () {
+        var title, info, dl_cnt;
+        for (var i = 0; i < this.books.length; i++) {
+            title = this.books[i].getElementsByTagName("h3")[0];
+            info = this.books[i].getElementsByTagName("dl")[0];
+            dl_cnt = info.getElementsByTagName("dd")[2].innerHTML;
+            dl_cnt = dl_cnt.split("回")[0];
+            dl_cnt = parseInt(dl_cnt);
+            // Popular Book
+            this.checkDLCount(dl_cnt, title);
+        };
+    }
+
+    checkDLCount (dl_cnt, tag) {
+        if (dl_cnt > 4000) {
+            tag.style.backgroundColor = "#ffcc99";
+        }
+    }
+};
+
+(function() {
+    markTitle = new MarkTitle();
+    markTitle.getInfo();
+    markTitle.mark();
+})();
