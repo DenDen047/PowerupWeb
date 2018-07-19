ec3920c7b50c7f290dce753702442e21b1a35a92
DenDen047 <sh.mn.nat@gmail.com>
2018-07-19 14:09:32 +0900

    update xbooks.js for search page

--- scripts/xbooks.js
+++ scripts/xbooks.js
@@ -17,20 +17,25 @@
     else {
         /*--- Search Page ---*/
         // top ad
-        ad_div.push(document.getElementById("main2col").getElementsByTagName('center')[0]);
+        ad_div.push(document.getElementById('wrap').getElementsByTagName("div")[0]);
+
         // banner bottom-right
         ad_div.push(document.getElementById('ad_tsuibi'));
-        // top article
-        var content_lists = document.getElementsByClassName("content_list");
-        ad_div.push(content_lists[0]);
-        // ad_div.push(content_lists[content_lists.length - 2]);
-        // preview box ad
-        ad_div.push(document.getElementsByClassName("box")[3]);
+        ad_div.push(document.getElementById('ad_tsuibi2'));
+
+        // in outline
+        ad_div.push(document.getElementById('main2col').getElementsByTagName('iframe')[0]);
+        ad_div = ad_div.concat(to_array(
+            document.getElementById('main2col').getElementsByTagName('center')));
     }
 
     // remove all
     for (var i = 0; i < ad_div.length; i++) {
         del_element(ad_div[i]);
+    }
+
+    function to_array(elements) {
+        return Array.prototype.slice.call(elements);
     }
 
     function del_element(element){
