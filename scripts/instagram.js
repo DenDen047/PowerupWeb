baa95d53bd82a710307368de722b361fddc0b6ca
DenDen047 <sh.mn.nat@gmail.com>
2018-10-12 10:02:33 +0900

    DOMの更新に応じて，scriptを実行し直す処理を追加

--- scripts/instagram.js
+++ scripts/instagram.js
@@ -34,10 +34,40 @@ var DownloadFunc = class {
     }
 };
 
+function dom_observe () {
+    const CLASS_NEW_TWEETS_BAR = "new-tweets-bar";
+
+    //監視ターゲットの取得
+    const target = document.querySelector(
+        "#react-root > section > main > section > div > div:nth-child(1) > div");
+
+    //オブザーバーの作成
+    const observer = new MutationObserver(records => {
+        console.log('load!!');
+        // records.forEach(record => {
+        //     Array.from(record.addedNodes)
+        //         .filter(node => node.classList.contains(CLASS_NEW_TWEETS_BAR))
+        //         .forEach(node => node.click());
+        // });
+    });
+
+    //監視オプションの作成
+    const options = {
+        childList: true
+    };
+
+    //監視の開始
+    observer.observe(target, options);
+}
+
 
 window.onload = function() {
     // document.write(document);
-    dl = new DownloadFunc();
+
+    // 画像のダウンロードボタンを生成する
+    var dl = new DownloadFunc();
     dl.createDLbutton();
 
+    // DOMの変更を監視して必要に応じて，更新する
+    dom_observe();
 }
