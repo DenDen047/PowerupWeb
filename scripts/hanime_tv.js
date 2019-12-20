454fd44620840369c544714e5cfebe313b345d65
DenDen047 <sh.mn.nat@gmail.com>
2019-12-20 19:28:05 +0900

    complete stateCheck()

--- scripts/hanime_tv.js
+++ scripts/hanime_tv.js
@@ -1,4 +1,8 @@
 var DownloadButtons = class {
+    constructor () {
+        this.img_lens = [];
+    }
+
     createDLLink () {
         var div = document.createElement('div');
 
@@ -6,15 +10,16 @@ var DownloadButtons = class {
     }
 
     createDLButton () {
+        console.log('go');
         // add DL buttons to each img element
-        for (var i=0, max=div_images.length; i < max; i++) {
-            // get the credit tag
-            var div_credit = div_images[i].getElementsByClassName('credits');
+        // for (var i=0, max=div_images.length; i < max; i++) {
+        //     // get the credit tag
+        //     var div_credit = div_images[i].getElementsByClassName('credits');
 
-            // add new element
-            var dl_link = this.createDLLink();
-            div_credit.appendChild(dl_link);
-        }
+        //     // add new element
+        //     var dl_link = this.createDLLink();
+        //     div_credit.appendChild(dl_link);
+        // }
 
     }
 
@@ -22,22 +27,35 @@ var DownloadButtons = class {
         // check the existence of the target div
         var div_images = document.getElementsByClassName(
             'cuc_container images__content flex row wrap justify-center relative');
-
         if (div_images.length == 0) {
-            console.log('wait');
-            return;
+            return false;
         }
-        else {
-            console.log('exit');
+
+        // check the number of images in the div
+        var as = div_images[0].getElementsByTagName('a');
+        if (as.length == 0) {
+            return false;
+        }
+
+        // check if the images can be load.
+        this.img_lens.push(as.length);
+        var max = this.img_lens.length;
+        if (this.img_lens[max - 3] == this.img_lens[max - 4]) {
+            return true;
         }
     }
 };
 
 
 (function() {
+    var dl = new DownloadButtons();
+    var runed = false;
     function run () {
-        dl = new DownloadButtons();
-        dl.stateCheck();
+        var ready = dl.stateCheck();
+        if (ready && !runed) {
+            dl.createDLButton();
+            runed = true;
+        }
     }
 
     document.addEventListener("DOMSubtreeModified", run);
