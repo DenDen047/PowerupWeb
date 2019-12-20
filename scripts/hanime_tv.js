e863a73109c592b62b907155be7da271999d0ac9
DenDen047 <sh.mn.nat@gmail.com>
2019-12-20 20:40:12 +0900

    fix the bug related to reload DOM

--- scripts/hanime_tv.js
+++ scripts/hanime_tv.js
@@ -3,6 +3,11 @@ var DownloadButtons = class {
         this.img_lens = [];
     }
 
+    getTargetDiv () {
+        return document.getElementsByClassName(
+            'cuc_container images__content flex row wrap justify-center relative');
+    }
+
     createDLLink () {
         var div = document.createElement('div');
 
@@ -10,23 +15,24 @@ var DownloadButtons = class {
     }
 
     createDLButton () {
-        console.log('go');
+        var div_images = this.getTargetDiv();
+        var as = div_images[0].getElementsByTagName('a');
+        console.log(as.length);
+
         // add DL buttons to each img element
-        // for (var i=0, max=div_images.length; i < max; i++) {
-        //     // get the credit tag
-        //     var div_credit = div_images[i].getElementsByClassName('credits');
+        for (var i=0, i_max=as.length; i < i_max; i++) {
+            // get the credit tag
+            var div_credit = as[i].getElementsByClassName('credits')[0];
 
-        //     // add new element
-        //     var dl_link = this.createDLLink();
-        //     div_credit.appendChild(dl_link);
-        // }
-
+            // add new element to each image
+            var dl_link = this.createDLLink();
+            div_credit.appendChild(dl_link);
+        }
     }
 
     stateCheck () {
         // check the existence of the target div
-        var div_images = document.getElementsByClassName(
-            'cuc_container images__content flex row wrap justify-center relative');
+        var div_images = this.getTargetDiv();
         if (div_images.length == 0) {
             return false;
         }
@@ -37,10 +43,10 @@ var DownloadButtons = class {
             return false;
         }
 
-        // check if the images can be load.
+        // check if all the images can be load.
         this.img_lens.push(as.length);
         var max = this.img_lens.length;
-        if (this.img_lens[max - 3] == this.img_lens[max - 4]) {
+        if (max > 4 && this.img_lens[max - 3] == this.img_lens[max - 4]) {
             return true;
         }
     }
@@ -50,11 +56,12 @@ var DownloadButtons = class {
 (function() {
     var dl = new DownloadButtons();
     var runed = false;
+
     function run () {
         var ready = dl.stateCheck();
         if (ready && !runed) {
-            dl.createDLButton();
             runed = true;
+            dl.createDLButton();
         }
     }
 
