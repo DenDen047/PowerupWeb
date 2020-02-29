56adffc15d3db8568aff6072e26e360955d471fa
DenDen047 <sh.mn.nat@gmail.com>
2020-02-29 10:01:00 +0900

    succeed to block related videos on youtube

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,43 +1,27 @@
 (function() {
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    // get URL
+    var url = document.URL;
+    url = url.split('/');
 
-    var observer = new MutationObserver(function() {
-        // get URL
-        var url = document.URL;
-        url = url.split('/');
+    console.log('HELLO WORLD');
 
-        // init
-        var ad_div = [];
+    // init
+    var blocked_contents = [];
 
-        // ad
-        // top
-        var ads = document.getElementsByClassName("header_big_pickup");
-        for (var i = 0; i < ads.length; i++) {
-            ad_div.push(ads[i]);
-        };
-        // bottom
-        var divs = document.getElementsByClassName("pict_rss");
-        var div = divs[divs.length-1];
-        ad_div.push(div);
-        // rss
-        ad_div.push(document.getElementById("rss_pickup_right"));
-        ad_div.push(document.getElementById("rss_pickup_left"));
+    // related videos
+    var related_videos = document.querySelector("#related")
+    blocked_contents.push(related_videos);
 
-        // remove alll
-        for (var i = 0; i < ad_div.length; i++) {
-            del_element(ad_div[i]);
-        }
+    // remove alll
+    for (var i = 0; i < blocked_contents.length; i++) {
+        del_element(blocked_contents[i]);
+    }
 
 
-        function del_element(element){
+    function del_element(element){
+        try {
             element.parentNode.removeChild(element);
         }
-    });
-
-    observer.observe(document, {
-        attributes: true,
-        childList: true,
-        characterData: true,
-        subtree:true
-    });
+        catch(e){}
+    }
 })();
